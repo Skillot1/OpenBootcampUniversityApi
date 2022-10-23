53f3673d97c083a249ae4e90962f39a33329ea50
Garci <garzi_06@hotmail.com>
2022-10-24 00:31:38 +0200

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        [Required]
+        public string DescripcionLarga { get; set; } = string.Empty;
+        [Required]
+        public string PublicoObjetivo { get; set; } = string.Empty;
+        [Required]
+        public string Objetivos { get; set; } = string.Empty;
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+        [Required]
+        public Nivel Nivel { get; set; }
+        
+    }
+
+    public enum Nivel { Basico, Intermedio, Avanzado }
+
+}
