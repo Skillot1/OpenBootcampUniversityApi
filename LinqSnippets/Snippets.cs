5b1e11fea581e3a2ed580bfe36bb05106a1417d1
Garci <garzi_06@hotmail.com>
2022-10-25 00:13:06 +0200

    Creado proyecto de LinqSnippets y clase Services de prueba con diversas búsquedas

---
+++ LinqSnippets/Snippets.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqSnippets
+{
+    public class Snippets
+    {
+        public static void BasicLinq()
+        {
+
+            string[] cars = { "WF Golf", "Audi A3", "Audi A5", "Fiat Punto", "Seat Ibiza", "Seat León" };
+
+            // 1. Select * from cars
+            var carlist = from car in cars select car;
+
+            //2. Select * from cars where Car in Audi
+
+            var audiList = from car in cars where car.Contains("Audi") select car;
+        }
+
+        public static void NumbersLinq()
+        {
+
+            List<int> numbers =new() { 1,2,3,4,5,6,7,8,9,10 };
+
+            // Devolver cada numero multiplicado por 3
+            //Devolver todos los números excepto el 9
+            //Ordenarlos de manera ascendente
+
+            var numbersX3 = numbers.Select(num => num*3)
+                .Where(x => x != 9)
+                .OrderBy(x => x)
+                .ToList();
+        }
+
+        public static void SearchExamples()
+        {
+            List<string> textList = new() {"a","bx","c","d","je","f","c" };
+
+            //Devolver el primer elemento
+
+          var first=  textList.First();
+
+            //Primer elemento que sea c
+
+         var firtsj=   textList.First(text=> text.Equals("c"));
+
+            //Primer elemento que contenga la j
+
+           var containsj= textList.First(text=> text.Contains("j"));
+
+            //Primer elemento que contenga la z o default -> Devuelve el elemento o un valor vacio.
+
+            var containsz = textList.FirstOrDefault(text => text.Contains("z"));
+
+            //Ultimo elemento que contenga la z o default -> Devuelve el elemento o un valor vacio.
+
+            var containsLastZ = textList.LastOrDefault(text => text.Contains("z"));
+
+            //Single values
+
+            var unique = textList.Single();
+            var uniqueOrDefault = textList.SingleOrDefault();
+
+            int[] evenNumbers = { 0, 2, 4, 6, 8 };
+            int[] otherNumbers = { 0, 2, 6 };
+
+            //Obtener los que no estén en el array de abajo
+            var myEvenNumbers =evenNumbers.Except(otherNumbers); //Devuelve 4,8
+
+        }
+
+        public static void MultipleSelects()
+        {
+            //Select many
+
+            string[] myOpinions = { "Opinion1, text1", "Opinion2, text2", "Opinion3, text3" };
+
+            //Hacer split
+            var myOpinionSelection = myOpinions.SelectMany(opinion => opinion.Split(","));
+
+            Console.WriteLine(myOpinionSelection);
+
+            //Operaciones con Enterprises
+
+            var enterprises = new[]
+            {
+                //Primer enterprise
+                new Enterprise()
+                {
+                    Id =1,
+                    Name ="Enterprise1",
+                    Employees = new[]
+                    {
+                        new Employee
+                        {
+                            Id=1,
+                            Name="Paco",
+                            Email ="paco@gmail.com",
+                            Salary = 2000
+                        },
+                          new Employee
+                        {
+                            Id=2,
+                            Name="Paca",
+                            Email ="pacapaca@gmail.com",
+                            Salary = 1000
+                        },
+                            new Employee
+                        {
+                            Id=3,
+                            Name="Pepa",
+                            Email ="pepa@gmail.com",
+                            Salary = 2200
+                        },
+                    }
+                },
+                //Segundo Enterprise
+                 new Enterprise()
+                {
+                    Id =2,
+                    Name ="Enterprise2",
+                    Employees = new[]
+                    {
+                        new Employee
+                        {
+                            Id=4,
+                            Name="Ana",
+                            Email ="Ana@gmail.com",
+                            Salary = 1500
+                        },
+                          new Employee
+                        {
+                            Id=5,
+                            Name="Juana",
+                            Email ="Juana@gmail.com",
+                            Salary = 1800
+                        },
+                            new Employee
+                        {
+                            Id=6,
+                            Name="Colombo",
+                            Email ="Colombo@gmail.com",
+                            Salary = 3100
+                        },
+                    }
+                }
+
+            };
+
+            // Obtener todos los empleados de todas las empresas
+
+            var employeeList = enterprises.SelectMany(e => e.Employees);
+
+            //Saber si cualquier lista esta vacia -> Comprobar si hay empresas // empleados
+
+            bool hasEnterprises = enterprises.Any();
+
+            bool hasEmployees = enterprises.Any(enterprise => enterprise.Employees.Any());
+
+            // Devolver los enterprises cuyos employees ganen al menos 2000 euros
+
+            bool hasEmployeesMore2000 = enterprises.Any(enterprise =>
+            enterprise.Employees.Any(employee=> employee.Salary>=2000));
+
+
+        }
+
+        public static void LinqCollections()
+        {
+            var firstList = new List<string>() { "a", "b", "c" };
+            var secondList = new List<string>() { "a", "c", "d" };
+
+            //Inner Join (elementos comunes a ambas listas) Dos posibilidades:
+
+            var commons = from element in firstList
+                          join secondsElement in secondList
+                          on element equals secondsElement
+                          select new { element, secondsElement };
+
+            var commonResult2 = firstList.Join(
+                secondList,
+                element => element,
+                secondElement => secondElement,
+                (element, secondsElement) => new { element, secondsElement }
+                );
+
+            //OuterJoin (Left/Right)
+
+            //Left Join
+            var LeftOuterJoin = from element in firstList
+                                join secondsElement in secondList
+                                on element equals secondsElement
+                                into temporalList //Hasta aquí el join normal, ahora hay que restar los comunes
+                                from temporalElement in temporalList.DefaultIfEmpty()
+                                where element != temporalElement
+                                select new { Element = element };
+
+
+            var LeftOuterJoin2 = from element in firstList
+                                 from secondsElement in secondList //Aqui ya tenemos el join normal
+                                 .Where(e => e == element).DefaultIfEmpty()
+                                 select new {Element = element, SecondElement=secondsElement} ;
+
+            //Right Join
+            var RightOuterJoin =
+                                from secondsElement in secondList 
+                                join element in firstList
+                                on secondsElement equals element
+                                into temporalList 
+                                from temporalElement in temporalList.DefaultIfEmpty()
+                                where secondsElement != temporalElement
+                                select new { SecondElement = secondsElement };
+
+            //Union
+
+            // var UnionList = LeftOuterJoin.Union(RightOuterJoin);
+
+        }
+
+        public static void SkipTakeLinq()
+        {
+            var myList = new[] {1,2,3,4,5};
+
+            //Saltar los dos primeros o los dos ultimos
+
+            var skip2 = myList.Skip(2); // 3,4,5
+
+            var skiplast2 = myList.SkipLast(2); // 1,2,3
+
+            //Saltar elementos con determinadas caracteristicas
+
+            var skipWhile = myList.SkipWhile(e => e<3);
+
+            //Funciones para coger determinados elementos
+
+            var take2 = myList.Take(2); //Coge los dos primeros
+
+            var takelast2 = myList.TakeLast(2); //Coge los dos ultimos
+
+            var takeWhileSmallerThan4 = myList.TakeWhile(e => e<4);
+        }
+
+        //Variables
+
+        //ZIP
+
+        //Repeat
+
+        //All
+
+        //Aggregate
+
+        //Distinct
+
+        //Group by
+
+
+
+
+
+    }
+}
---
+++ UniversityApiBackend/Services/Services.cs
@@ -0,0 +1,61 @@
+﻿using UniversityApiBackend.DataAccess;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public class Services
+    {
+        //Entiendo que sería mejor crear una interfaz para cada modelo con los métodos requeridos y luego hacer clases que hereden de ella, pero al ser un popurrí, lo hago directamente aquí
+
+        private readonly UniversityDBContext _context;
+
+        public Services(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        //Buscar usuarios por email
+
+       public  User FindUserByEmail(string email)
+        {
+            return _context.Users.Where(u => u.Email == email).FirstOrDefault();
+        }
+
+        //Buscar alumnos mayores de edad
+
+        public List<Student> FindAdultStudents()
+        {
+            return _context.Students.Where(student=> (DateTime.Now.Year- student.DateOfBirth.Year) >=18).ToList();
+        }
+
+        //Buscar alumnos que tengan al menos un curso
+
+        public List<Student> FindStudentsWithCourses()
+        {
+            return _context.Students.Where(student => student.Courses.Any()).ToList();
+        }
+
+        //Buscar cursos de un nivel determinado que al menos tengan un alumno inscrito
+
+        public List<Course> FindCoursesByLevelWithStudents(Level level)
+        {
+            return _context.Courses.Where(course => course.Level == level && course.Students.Any()).ToList();
+        }
+
+        //Buscar cursos de un nivel determinado que sean de una categoría determinada
+        public List<Course> FindCoursesByLevelAndCategory(Level level, Category category)
+        {
+            return _context.Courses.Where(course => course.Level == level && course.Categories.Any(cat => cat ==  category)).ToList();
+        }
+
+
+        //Buscar cursos sin alumnos
+
+        public List<Course> FindCourseslWithoutStudents()
+        {
+            return _context.Courses.Where(course => course.Students.Any()).ToList();
+        }
+
+
+    }
+}
